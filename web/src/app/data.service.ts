5d30c6c0cd75a0ae24162ac978e8a34c8e027064
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-08-01 21:46:24 +0300

    #transenix  added Angular Perfect Scrollbar to terminal

--- web/src/app/data.service.ts
+++ web/src/app/data.service.ts
@@ -13,353 +13,6 @@ export class DataService {
   private groupUrl = 'http://map1.mobo.cards:8093/api/v1/service-groups';
   private transactionUrl = 'http://map1.mobo.cards:8093/api/v1/transactions';
 
-  // terminals = [
-  //   {
-  //     "terminalId": "cb22bb62-0d1f-4228-8440-276819bf12d4",
-  //     "groupNumber": 1,
-  //     "opPurchase": "N",
-  //     "opReversal": "Y",
-  //     "opRefund": "Y",
-  //     "manual": "Y",
-  //     "pin": "Y",
-  //     "geoPosition": "Y",
-  //     "limitVisa": 100000,
-  //     "limitMc": 50000,
-  //     "limitProstir": 100000,
-  //     "visaAccepted": "Y",
-  //     "mcAccepted": "Y",
-  //     "prostirAccepted": "Y",
-  //     "receiptTemplate": 1,
-  //     "configChanged": "N",
-  //     "merchant": {
-  //       "merchantId": "0d385b77-13e3-4453-ac32-acace7e2c7ac",
-  //       "legal_name": "ТОВ.Сонечко",
-  //       "merchantName": "COFEE SHOP VESELKA",
-  //       "merchantLocation": "25-A PRORIZNA ST.",
-  //       "taxId": 1235847895,
-  //       "mcc": 6012,
-  //       "acquirerId": "42562801"
-  //     },
-  //     "allowedLanguages": [
-  //       {
-  //         "languageId": "UKR"
-  //       },
-  //       {
-  //         "languageId": "RUS"
-  //       },
-  //       {
-  //         "languageId": "ENG"
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //     "groupNumber": 1,
-  //     "opPurchase": "Y",
-  //     "opReversal": "Y",
-  //     "opRefund": "Y",
-  //     "manual": "Y",
-  //     "pin": "Y",
-  //     "geoPosition": "Y",
-  //     "limitVisa": 100000,
-  //     "limitMc": 50000,
-  //     "limitProstir": 100000,
-  //     "visaAccepted": "Y",
-  //     "mcAccepted": "Y",
-  //     "prostirAccepted": "Y",
-  //     "receiptTemplate": 1,
-  //     "configChanged": "N",
-  //     "merchant": {
-  //       "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //       "legal_name": "ТОВ.",
-  //       "merchantName": "COFEE SHOP VESELKA",
-  //       "merchantLocation": "25-A PRORIZNA ST.",
-  //       "taxId": 1235847895,
-  //       "mcc": 6012,
-  //       "acquirerId": "42562801"
-  //     },
-  //     "allowedLanguages": [
-  //       {
-  //         "languageId": "UKR"
-  //       },
-  //       {
-  //         "languageId": "RUS"
-  //       },
-  //       {
-  //         "languageId": "ENG"
-  //       }
-  //     ]
-  //   },
-  //
-  //
-  //   // {
-  //   //   "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //   //   "groupNumber": 1,
-  //   //   "opPurchase": "Y",
-  //   //   "opReversal": "Y",
-  //   //   "opRefund": "Y",
-  //   //   "manual": "Y",
-  //   //   "pin": "Y",
-  //   //   "geoPosition": "Y",
-  //   //   "limitVisa": 100000,
-  //   //   "limitMc": 50000,
-  //   //   "limitProstir": 100000,
-  //   //   "visaAccepted": "Y",
-  //   //   "mcAccepted": "Y",
-  //   //   "prostirAccepted": "Y",
-  //   //   "receiptTemplate": 1,
-  //   //   "configChanged": "N",
-  //   //   "merchant": {
-  //   //     "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //   //     "legal_name": "ТОВ.",
-  //   //     "merchantName": "COFEE SHOP VESELKA",
-  //   //     "merchantLocation": "25-A PRORIZNA ST.",
-  //   //     "taxId": 1235847895,
-  //   //     "mcc": 6012,
-  //   //     "acquirerId": "42562801"
-  //   //   },
-  //   //   "allowedLanguages": [
-  //   //     {
-  //   //       "languageId": "UKR"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "RUS"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "ENG"
-  //   //     }
-  //   //   ]
-  //   // },
-  //   // {
-  //   //   "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //   //   "groupNumber": 1,
-  //   //   "opPurchase": "Y",
-  //   //   "opReversal": "Y",
-  //   //   "opRefund": "Y",
-  //   //   "manual": "Y",
-  //   //   "pin": "Y",
-  //   //   "geoPosition": "Y",
-  //   //   "limitVisa": 100000,
-  //   //   "limitMc": 50000,
-  //   //   "limitProstir": 100000,
-  //   //   "visaAccepted": "Y",
-  //   //   "mcAccepted": "Y",
-  //   //   "prostirAccepted": "Y",
-  //   //   "receiptTemplate": 1,
-  //   //   "configChanged": "N",
-  //   //   "merchant": {
-  //   //     "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //   //     "legal_name": "ТОВ.",
-  //   //     "merchantName": "COFEE SHOP VESELKA",
-  //   //     "merchantLocation": "25-A PRORIZNA ST.",
-  //   //     "taxId": 1235847895,
-  //   //     "mcc": 6012,
-  //   //     "acquirerId": "42562801"
-  //   //   },
-  //   //   "allowedLanguages": [
-  //   //     {
-  //   //       "languageId": "UKR"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "RUS"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "ENG"
-  //   //     }
-  //   //   ]
-  //   // },
-  //   // {
-  //   //   "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //   //   "groupNumber": 1,
-  //   //   "opPurchase": "Y",
-  //   //   "opReversal": "Y",
-  //   //   "opRefund": "Y",
-  //   //   "manual": "Y",
-  //   //   "pin": "Y",
-  //   //   "geoPosition": "Y",
-  //   //   "limitVisa": 100000,
-  //   //   "limitMc": 50000,
-  //   //   "limitProstir": 100000,
-  //   //   "visaAccepted": "Y",
-  //   //   "mcAccepted": "Y",
-  //   //   "prostirAccepted": "Y",
-  //   //   "receiptTemplate": 1,
-  //   //   "configChanged": "N",
-  //   //   "merchant": {
-  //   //     "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //   //     "legal_name": "ТОВ.",
-  //   //     "merchantName": "COFEE SHOP VESELKA",
-  //   //     "merchantLocation": "25-A PRORIZNA ST.",
-  //   //     "taxId": 1235847895,
-  //   //     "mcc": 6012,
-  //   //     "acquirerId": "42562801"
-  //   //   },
-  //   //   "allowedLanguages": [
-  //   //     {
-  //   //       "languageId": "UKR"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "RUS"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "ENG"
-  //   //     }
-  //   //   ]
-  //   // },
-  //   // {
-  //   //   "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //   //   "groupNumber": 1,
-  //   //   "opPurchase": "Y",
-  //   //   "opReversal": "Y",
-  //   //   "opRefund": "Y",
-  //   //   "manual": "Y",
-  //   //   "pin": "Y",
-  //   //   "geoPosition": "Y",
-  //   //   "limitVisa": 100000,
-  //   //   "limitMc": 50000,
-  //   //   "limitProstir": 100000,
-  //   //   "visaAccepted": "Y",
-  //   //   "mcAccepted": "Y",
-  //   //   "prostirAccepted": "Y",
-  //   //   "receiptTemplate": 1,
-  //   //   "configChanged": "N",
-  //   //   "merchant": {
-  //   //     "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //   //     "legal_name": "ТОВ.",
-  //   //     "merchantName": "COFEE SHOP VESELKA",
-  //   //     "merchantLocation": "25-A PRORIZNA ST.",
-  //   //     "taxId": 1235847895,
-  //   //     "mcc": 6012,
-  //   //     "acquirerId": "42562801"
-  //   //   },
-  //   //   "allowedLanguages": [
-  //   //     {
-  //   //       "languageId": "UKR"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "RUS"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "ENG"
-  //   //     }
-  //   //   ]
-  //   // },
-  //   // {
-  //   //   "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //   //   "groupNumber": 1,
-  //   //   "opPurchase": "Y",
-  //   //   "opReversal": "Y",
-  //   //   "opRefund": "Y",
-  //   //   "manual": "Y",
-  //   //   "pin": "Y",
-  //   //   "geoPosition": "Y",
-  //   //   "limitVisa": 100000,
-  //   //   "limitMc": 50000,
-  //   //   "limitProstir": 100000,
-  //   //   "visaAccepted": "Y",
-  //   //   "mcAccepted": "Y",
-  //   //   "prostirAccepted": "Y",
-  //   //   "receiptTemplate": 1,
-  //   //   "configChanged": "N",
-  //   //   "merchant": {
-  //   //     "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //   //     "legal_name": "ТОВ.",
-  //   //     "merchantName": "COFEE SHOP VESELKA",
-  //   //     "merchantLocation": "25-A PRORIZNA ST.",
-  //   //     "taxId": 1235847895,
-  //   //     "mcc": 6012,
-  //   //     "acquirerId": "42562801"
-  //   //   },
-  //   //   "allowedLanguages": [
-  //   //     {
-  //   //       "languageId": "UKR"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "RUS"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "ENG"
-  //   //     }
-  //   //   ]
-  //   // },
-  //   // {
-  //   //   "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //   //   "groupNumber": 1,
-  //   //   "opPurchase": "Y",
-  //   //   "opReversal": "Y",
-  //   //   "opRefund": "Y",
-  //   //   "manual": "Y",
-  //   //   "pin": "Y",
-  //   //   "geoPosition": "Y",
-  //   //   "limitVisa": 100000,
-  //   //   "limitMc": 50000,
-  //   //   "limitProstir": 100000,
-  //   //   "visaAccepted": "Y",
-  //   //   "mcAccepted": "Y",
-  //   //   "prostirAccepted": "Y",
-  //   //   "receiptTemplate": 1,
-  //   //   "configChanged": "N",
-  //   //   "merchant": {
-  //   //     "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //   //     "legal_name": "ТОВ.",
-  //   //     "merchantName": "COFEE SHOP VESELKA",
-  //   //     "merchantLocation": "25-A PRORIZNA ST.",
-  //   //     "taxId": 1235847895,
-  //   //     "mcc": 6012,
-  //   //     "acquirerId": "42562801"
-  //   //   },
-  //   //   "allowedLanguages": [
-  //   //     {
-  //   //       "languageId": "UKR"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "RUS"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "ENG"
-  //   //     }
-  //   //   ]
-  //   // },
-  //   // {
-  //   //   "terminalId": "629275d3-d47f-45a4-8f7d-d3929bbeecca",
-  //   //   "groupNumber": 1,
-  //   //   "opPurchase": "Y",
-  //   //   "opReversal": "Y",
-  //   //   "opRefund": "Y",
-  //   //   "manual": "Y",
-  //   //   "pin": "Y",
-  //   //   "geoPosition": "Y",
-  //   //   "limitVisa": 100000,
-  //   //   "limitMc": 50000,
-  //   //   "limitProstir": 100000,
-  //   //   "visaAccepted": "Y",
-  //   //   "mcAccepted": "Y",
-  //   //   "prostirAccepted": "Y",
-  //   //   "receiptTemplate": 1,
-  //   //   "configChanged": "N",
-  //   //   "merchant": {
-  //   //     "merchantId": "f6ffb4b4-44f0-494d-9d2b-031f1cc03504",
-  //   //     "legal_name": "ТОВ.",
-  //   //     "merchantName": "COFEE SHOP VESELKA",
-  //   //     "merchantLocation": "25-A PRORIZNA ST.",
-  //   //     "taxId": 1235847895,
-  //   //     "mcc": 6012,
-  //   //     "acquirerId": "42562801"
-  //   //   },
-  //   //   "allowedLanguages": [
-  //   //     {
-  //   //       "languageId": "UKR"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "RUS"
-  //   //     },
-  //   //     {
-  //   //       "languageId": "ENG"
-  //   //     }
-  //   //   ]
-  //   // }
-  // ];
-
   terminals = {
     "content": [
       {
@@ -560,6 +213,193 @@ export class DataService {
         "configChanged": "N",
         "merchant": {
           "merchantId": "f8b4a888-07cf-4c32-83dc-4384810f4ddc",
+          "merchantName": "COFEE SHOP VESELKA",
+          "merchantLocation": "25-A PRORIZNA ST.",
+          "taxId": 1235847895,
+          "mcc": 6012,
+          "acquirerId": "42562801"
+        },
+        "allowedLanguages": [
+          {
+            "languageId": "UKR"
+          },
+          {
+            "languageId": "RUS"
+          },
+          {
+            "languageId": "ENG"
+          }
+        ]
+      },
+      {
+        "terminalId": "5180b5bf",
+        "groupNumber": 1,
+        "opPurchase": "Y",
+        "opReversal": "Y",
+        "opRefund": "Y",
+        "manual": "Y",
+        "pin": "Y",
+        "geoPosition": "Y",
+        "limitVisa": 100000,
+        "limitMc": 50000,
+        "limitProstir": 100000,
+        "visaAccepted": "Y",
+        "mcAccepted": "Y",
+        "prostirAccepted": "Y",
+        "receiptTemplate": 1,
+        "configChanged": "N",
+        "merchant": {
+          "merchantId": "91f05e13-e5a7-4f26-9a75-c5336ebfd6c7",
+          "merchantName": "COFEE SHOP VESELKA",
+          "merchantLocation": "25-A PRORIZNA ST.",
+          "taxId": 1235847895,
+          "mcc": 6012,
+          "acquirerId": "42562801"
+        },
+        "allowedLanguages": [
+          {
+            "languageId": "UKR"
+          },
+          {
+            "languageId": "RUS"
+          },
+          {
+            "languageId": "ENG"
+          }
+        ]
+      },
+
+
+      {
+        "terminalId": "5180b5bf",
+        "groupNumber": 1,
+        "opPurchase": "Y",
+        "opReversal": "Y",
+        "opRefund": "Y",
+        "manual": "Y",
+        "pin": "Y",
+        "geoPosition": "Y",
+        "limitVisa": 100000,
+        "limitMc": 50000,
+        "limitProstir": 100000,
+        "visaAccepted": "Y",
+        "mcAccepted": "Y",
+        "prostirAccepted": "Y",
+        "receiptTemplate": 1,
+        "configChanged": "N",
+        "merchant": {
+          "merchantId": "91f05e13-e5a7-4f26-9a75-c5336ebfd6c7",
+          "merchantName": "COFEE SHOP VESELKA",
+          "merchantLocation": "25-A PRORIZNA ST.",
+          "taxId": 1235847895,
+          "mcc": 6012,
+          "acquirerId": "42562801"
+        },
+        "allowedLanguages": [
+          {
+            "languageId": "UKR"
+          },
+          {
+            "languageId": "RUS"
+          },
+          {
+            "languageId": "ENG"
+          }
+        ]
+      },
+      {
+        "terminalId": "5180b5bf",
+        "groupNumber": 1,
+        "opPurchase": "Y",
+        "opReversal": "Y",
+        "opRefund": "Y",
+        "manual": "Y",
+        "pin": "Y",
+        "geoPosition": "Y",
+        "limitVisa": 100000,
+        "limitMc": 50000,
+        "limitProstir": 100000,
+        "visaAccepted": "Y",
+        "mcAccepted": "Y",
+        "prostirAccepted": "Y",
+        "receiptTemplate": 1,
+        "configChanged": "N",
+        "merchant": {
+          "merchantId": "91f05e13-e5a7-4f26-9a75-c5336ebfd6c7",
+          "merchantName": "COFEE SHOP VESELKA",
+          "merchantLocation": "25-A PRORIZNA ST.",
+          "taxId": 1235847895,
+          "mcc": 6012,
+          "acquirerId": "42562801"
+        },
+        "allowedLanguages": [
+          {
+            "languageId": "UKR"
+          },
+          {
+            "languageId": "RUS"
+          },
+          {
+            "languageId": "ENG"
+          }
+        ]
+      },
+      {
+        "terminalId": "5180b5bf",
+        "groupNumber": 1,
+        "opPurchase": "Y",
+        "opReversal": "Y",
+        "opRefund": "Y",
+        "manual": "Y",
+        "pin": "Y",
+        "geoPosition": "Y",
+        "limitVisa": 100000,
+        "limitMc": 50000,
+        "limitProstir": 100000,
+        "visaAccepted": "Y",
+        "mcAccepted": "Y",
+        "prostirAccepted": "Y",
+        "receiptTemplate": 1,
+        "configChanged": "N",
+        "merchant": {
+          "merchantId": "91f05e13-e5a7-4f26-9a75-c5336ebfd6c7",
+          "merchantName": "COFEE SHOP VESELKA",
+          "merchantLocation": "25-A PRORIZNA ST.",
+          "taxId": 1235847895,
+          "mcc": 6012,
+          "acquirerId": "42562801"
+        },
+        "allowedLanguages": [
+          {
+            "languageId": "UKR"
+          },
+          {
+            "languageId": "RUS"
+          },
+          {
+            "languageId": "ENG"
+          }
+        ]
+      },
+      {
+        "terminalId": "5180b5bf",
+        "groupNumber": 1,
+        "opPurchase": "Y",
+        "opReversal": "Y",
+        "opRefund": "Y",
+        "manual": "Y",
+        "pin": "Y",
+        "geoPosition": "Y",
+        "limitVisa": 100000,
+        "limitMc": 50000,
+        "limitProstir": 100000,
+        "visaAccepted": "Y",
+        "mcAccepted": "Y",
+        "prostirAccepted": "Y",
+        "receiptTemplate": 1,
+        "configChanged": "N",
+        "merchant": {
+          "merchantId": "91f05e13-e5a7-4f26-9a75-c5336ebfd6c7",
           "merchantName": "COFEE SHOP VESELKA",
           "merchantLocation": "25-A PRORIZNA ST.",
           "taxId": 1235847895,
