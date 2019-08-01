15b35d09e246c6f0bef9c8f61c4b634325c49336
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-08-01 15:50:51 +0300

    #transenix  fix FormsModule

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
@@ -34,8 +34,8 @@ import { TokenInterceptor } from "./core/interceptor";
   ],
   imports: [
     BrowserModule,
-    // routing,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
